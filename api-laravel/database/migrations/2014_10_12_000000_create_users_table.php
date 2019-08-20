b50086087b222998293ccb9d635c35cfecab1e4e
Bruna Klaumann <42134@UNIDAVI.local>
2019-08-20 20:51:04 -0300

    token validado

--- api-laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ api-laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email', 100)->unique();
+            $table->string('api_token')->unique()-> nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- api-laravel/routes/api.php
+++ api-laravel/routes/api.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Http\Request;
 Use \App\Article;
-
-Route::get("articles", 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{id}', 'ArticleController@update');
-Route::delete('articles/{id}', 'ArticleController@delete');
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{id}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{id}', 'ArticleController@update');
+    Route::delete('articles/{id}', 'ArticleController@delete');
+});
+Route::post("register", 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+?>
